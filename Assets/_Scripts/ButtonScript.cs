9913a5a40047a7ea96ea3ba050535b5198a44b33
jacobshenning <jacobshenning@gmail.com>
2016-04-13 09:26:43 -0700

    First Upload
    yup

---
+++ Assets/_Scripts/ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ButtonScript : NetworkBehaviour 
+{
+    
+    public GameObject gameController;
+    
+	[Command]
+    public void Cmd_Toggle()
+    {
+		gameController = GameObject.FindGameObjectWithTag ("GameController");
+    }
+}
