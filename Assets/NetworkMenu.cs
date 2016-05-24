1e50cc22debfb834376974288dc3bf56b38c3e4b
William Naslund <12150w@gmail.com>
2016-05-24 03:15:20 -0600

    Added game name text input

--- Assets/NetworkMenu.cs
+++ Assets/NetworkMenu.cs
@@ -11,12 +11,15 @@ public class NetworkMenu : MonoBehaviour {
     // StatusText is the network loading status text
     public Text StatusText;
 
+    // GameName is the name of the game if hosting
+    public Text GameName;
+
     // Finder is the game's network finder
     public NetworkGameFinder Finder;
 
     // HostGame starts hosting a game
     public void HostGame() {
-        bool started = Finder.StartHosting("Something 123");
+        bool started = Finder.StartHosting(GameName.text);
         
         if(started != true) {
             Debug.LogError("Failed to start hosting game");
